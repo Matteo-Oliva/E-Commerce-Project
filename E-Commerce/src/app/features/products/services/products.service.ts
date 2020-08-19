072c0c4f35d4a696d44c5b7f856320e680258051
Carmelo <melonemark8@gmail.com>
2020-08-19 22:14:36 +0200

    add Service

---
+++ E-Commerce/src/app/features/products/services/products.service.ts
@@ -0,0 +1,18 @@
+import { Product } from './../../../shared/model/product';
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  
+  private productsUrl = 'api/products';
+
+  constructor( private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
+}
---
+++ E-Commerce/src/app/shared/model/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id:number,
+    title:string,
+    imgPath:string,
+    description:string,
+    price:number,
+    review:number
+}
