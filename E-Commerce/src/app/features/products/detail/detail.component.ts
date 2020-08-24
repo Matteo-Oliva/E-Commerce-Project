29156766d78e66c22dcbc75d57e52dd0959e4b12
Carmelo <melonemark8@gmail.com>
2020-08-24 16:37:48 +0200

    add function

--- E-Commerce/src/app/features/products/detail/detail.component.ts
+++ E-Commerce/src/app/features/products/detail/detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../../../shared/model/product';
+import { ProductsService } from './../services/products.service';
+import { Component, OnInit,Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+product: Product;
+
+ constructor(private productsService:ProductsService,
+             private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  getProduct():void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productsService.getProduct(id)
+    .subscribe(product => this.product = product)
+ }
 }
--- E-Commerce/src/app/features/products/services/products.service.ts
+++ E-Commerce/src/app/features/products/services/products.service.ts
@@ -7,12 +7,17 @@ import { Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class ProductsService {
-
+  
   private productsUrl = 'api/products';
 
   constructor( private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl);
+  }
+
+  getProduct(id:number): Observable<Product> {
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(url);
   }
 }
