94ba12dca0a37f44482fcbbdbda075e93322e5ee
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-08-30 18:34:11 +0200

    Cart fixes

--- E-Commerce/src/app/features/basket/cart/cart.component.ts
+++ E-Commerce/src/app/features/basket/cart/cart.component.ts
@@ -1,7 +1,9 @@
 import { Product } from './../../../shared/model/product';
-import { ProductsService } from './../../products/services/products.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { CartService } from '../service/cart.service';
+import { Identifiers } from '@angular/compiler';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-cart',
@@ -11,11 +13,58 @@ import { CartService } from '../service/cart.service';
 export class CartComponent implements OnInit {
   
   products:Product[] ;
+  items;
+  itemQuantity = 1;
  
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.products = this.cartService.getItems();
+    this.items = this.cartService.getItems();
+  
   }
 
+  addToCart(product): void {
+    this.products.push(product);
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+
+plus = function(item) {
+  item = item++;
 }
+
+minus = function(item) {
+  item = item--;
+}
+
+add(product){
+
+  this.items.push(product)
+}
+
+total() {
+  return this.items.reduce((total, item) => total + item.price, 0);
+}
+
+remove(product) {
+  this.items.splice(product,1);
+}
+
+
+addQuantity(){
+
+    this.itemQuantity++;
+}
+
+
+
+
+
+
+}
+
+
--- E-Commerce/src/app/features/basket/service/cart.service.ts
+++ E-Commerce/src/app/features/basket/service/cart.service.ts
@@ -20,5 +20,7 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+ 
   
 }
